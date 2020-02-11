fcbb988ae5c040ac0f95df30ccb50098bb8525c5
alexei.melnic <alexei.melnic@hpe.com>
2020-02-11 20:35:14 +0200

    initial project

---
+++ src/test/java/md/cantina/CantinaApplicationTests.java
@@ -0,0 +1,13 @@
+package md.cantina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CantinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
